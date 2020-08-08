d4f6c7134452e3b96a93e9246c15712ca2ce42e6
Neeraj Kumar <kansalneeraj9194@gmail.com>
2020-08-09 00:32:25 +0530

    Teacher edit feature added.

--- teacher/urls.py
+++ teacher/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from teacher.views import TeacherView
 
 urlpatterns = [
-    path('', TeacherView.as_view()),
+    path('', TeacherView.as_view(), name="list"),
+    path('<int:pk>/', TeacherView.as_view(), name="detail")
 ]
