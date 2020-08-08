9535859e9b3a1081b8f240ee2f0b2620fac34d65
Neeraj Kumar <kansalneeraj9194@gmail.com>
2020-08-09 00:14:14 +0530

    Student Edit/Detail View with template changes.

--- student/urls.py
+++ student/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from student.views import StudentView
 
 urlpatterns = [
-    path('', StudentView.as_view(template_name="student/student.html")),
+    path('', StudentView.as_view(), name="list"),
+    path('<int:pk>/', StudentView.as_view(), name="detail")
 ]
--- student/views.py
+++ student/views.py
@@ -1,15 +1,26 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from student.forms import StudentForm
 from student.models import Student
 
 
-class StudentView(CreateView):
+class StudentView(CreateView, UpdateView):
     form_class = StudentForm
     model = Student
     success_url = '/student/'
+    template_name = "student/student.html"
 
     def get_context_data(self, **kwargs):
         kwargs['object_list'] = Student.objects.all()
         return super(StudentView, self).get_context_data(**kwargs)
+
+    def get(self, request, *args, **kwargs):
+        """
+        Update "get" to accept both URLs.
+        """
+        if kwargs.get('pk'):
+            self.object = self.get_object()
+        else:
+            self.object = None
+        return self.render_to_response(self.get_context_data())
 
